6fec3fc8e4b57fde5a6e04680c80f53393a5cd2c
William Stevens <wastevensv@gmail.com>
2016-06-13 20:45:28 -0400

    Added editing and deleting of notes.

--- postdrop/webserv.py
+++ postdrop/webserv.py
@@ -16,7 +16,7 @@ def index():
     return "Not implemented."
 
 @app.route('/note/<shorturl>')
-def note(shorturl):
+def view_note(shorturl):
     note_id, user_id = Note.fromshorturl(shorturl)
     note = Note.query.filter_by(id=note_id, owner_id=user_id).first()
     if note is not None:
@@ -32,17 +32,61 @@ def note(shorturl):
     else:
         return "No such note.", 404
 
+@app.route('/note/<shorturl>/delete', methods=['GET','POST'])
+def delete_note(shorturl):
+    note_id, user_id = Note.fromshorturl(shorturl)
+    owner = User.query.filter_by(id=user_id).first()
+    if request.method == 'POST':
+        auth = request.json['auth']
+        if not owner.verify_auth_key(auth): return 'Bad Auth', 403
+        Note.query.filter_by(id=note_id, owner_id=user_id).delete()
+        return "Successful"
+    else:
+        return "Not implemented."
+
+@app.route('/note/<shorturl>/edit', methods=['GET','POST'])
+def edit_note(shorturl):
+    note_id, user_id = Note.fromshorturl(shorturl)
+    note = Note.query.filter_by(id=note_id, owner_id=user_id).first()
+    owner = User.query.filter_by(id=user_id).first()
+    if request.method == 'POST' and note is not None:
+        auth = request.json['auth']
+        if not owner.verify_auth_key(auth): return 'Bad Auth', 403
+
+        if 'title' in request.json:
+            note.title = request.json['title']
+        if 'text' in request.json:
+            note.text = request.json['text']
+        if 'tags' in request.json:
+            for name in request.json['tags']:
+                tag = Tag.query.filter_by(name=name).first()
+                if tag is None:
+                    tag = Tag(name=name)
+                    db.add(tag)
+                note.tags.append(tag)
+
+        db.add(note)
+        db.commit()
+        return note.shorturl()
+    else:
+        return "Not implemented."
+
 @app.route('/note/new', methods=['GET','POST'])
 def new_note():
     if request.method == 'POST':
+        if 'username' not in request.json:
+            return 'Username required.', 400
         username = request.json['username']
         auth = request.json['auth']
         owner = User.query.filter_by(username=username).first()
         if not owner.verify_auth_key(auth): return 'Bad Auth', 403
 
+        if 'title' not in request.json:
+            return 'Title required', 400
         title = request.json['title']
-        text = request.json['text']
-        note = Note(title=title, text=text, owner=owner)
+        note = Note(title=title, owner=owner)
+        if 'text' in request.json:
+            note.text = request.json['text']
         for name in request.json['tags']:
             tag = Tag.query.filter_by(name=name).first()
             if tag is None:
@@ -68,6 +112,7 @@ def search_tag(name):
             'title': note.title,
             'text': note.text,
             'owner': note.owner.username,
+            'shorturl': note.shorturl(),
             'tags': tags
         })
     return jsonify({'notes':notes})
